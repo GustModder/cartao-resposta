78064d5f1c8d057e31a3b7e8b60e1bae8eacc1c7
GustModder <110644508+GustModder@users.noreply.github.com>
2024-10-25 08:26:47 -0300

    e

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quantos continentes existem no mundo?',
+    'sete'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Sistema solar',
+    'Qual o maior planeta do sistema solar?',
+    'Jupiter'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qimica',
+    'Qual 0 simbolo quimico do oxigenio?',
+    'O'
+)
+
+criaCartao(
+    ' ciencias',
+    'O que e fotossintese?',
+    'E o processo pelo qual as plantas produzem alimentos usanso a luz solar'
 )
